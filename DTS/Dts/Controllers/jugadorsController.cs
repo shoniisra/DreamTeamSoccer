a0f2938da1e2bbb0d52d99fd78a6d51e90edbdef
israel villacis <villajohnny@gmail.com>
2018-12-06 23:12:59 -0500

    Backup antes de cambiar layout

---
+++ DTS/Dts/Controllers/jugadorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dts;
+
+namespace Dts.Controllers
+{
+    public class jugadorsController : Controller
+    {
+        private soccerdbEntities db = new soccerdbEntities();
+
+        // GET: jugadors
+        public ActionResult Index()
+        {
+            return View(db.jugadors.ToList());
+        }
+
+        // GET: jugadors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            jugador jugador = db.jugadors.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // GET: jugadors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: jugadors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "jugid,jugnombre,jugfechanac,jugsexo,jugposicion,jugreputacion,jugemail,jugcontraseña")] jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.jugadors.Add(jugador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(jugador);
+        }
+
+        // GET: jugadors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            jugador jugador = db.jugadors.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // POST: jugadors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "jugid,jugnombre,jugfechanac,jugsexo,jugposicion,jugreputacion,jugemail,jugcontraseña")] jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jugador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(jugador);
+        }
+
+        // GET: jugadors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            jugador jugador = db.jugadors.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // POST: jugadors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            jugador jugador = db.jugadors.Find(id);
+            db.jugadors.Remove(jugador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DTS/Dts/Controllers/partidoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dts;
+
+namespace Dts.Controllers
+{
+    public class partidoesController : Controller
+    {
+        private soccerdbEntities db = new soccerdbEntities();
+
+        // GET: partidoes
+        public ActionResult Index()
+        {
+            var partidoes = db.partidoes.Include(p => p.cancha);
+            return View(partidoes.ToList());
+        }
+
+        // GET: partidoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            partido partido = db.partidoes.Find(id);
+            if (partido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partido);
+        }
+
+        // GET: partidoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.cancha_idcancha = new SelectList(db.canchas, "idcancha", "cannombre");
+            return View();
+        }
+
+        // POST: partidoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "parid,partitulo,parhora,cancha_idcancha")] partido partido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.partidoes.Add(partido);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cancha_idcancha = new SelectList(db.canchas, "idcancha", "cannombre", partido.cancha_idcancha);
+            return View(partido);
+        }
+
+        // GET: partidoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            partido partido = db.partidoes.Find(id);
+            if (partido == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cancha_idcancha = new SelectList(db.canchas, "idcancha", "cannombre", partido.cancha_idcancha);
+            return View(partido);
+        }
+
+        // POST: partidoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "parid,partitulo,parhora,cancha_idcancha")] partido partido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(partido).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cancha_idcancha = new SelectList(db.canchas, "idcancha", "cannombre", partido.cancha_idcancha);
+            return View(partido);
+        }
+
+        // GET: partidoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            partido partido = db.partidoes.Find(id);
+            if (partido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partido);
+        }
+
+        // POST: partidoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            partido partido = db.partidoes.Find(id);
+            db.partidoes.Remove(partido);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
